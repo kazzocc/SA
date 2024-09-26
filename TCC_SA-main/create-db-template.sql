30f02029110c35cbc2a461a5b8b09c81ab21b8f9
Kaz_zoc <64361796+kazzocc@users.noreply.github.com>
2024-09-26 00:08:16 -0300

    Delete TCC_SA-main directory

--- TCC_SA-main/create-db-template.sql
+++
@@ -1,9 +0,0 @@
-create database tccSA;
-use tccSA;
-create table tb_usuarios (
-	id int PRIMARY KEY AUTO_INCREMENT,
-	nome  varchar(100),
-    email varchar(255),
-    senha varchar(100),
-	tipo integer -- 0 para "aluno" e 1 para "professor" e 2 para servidor
-);
